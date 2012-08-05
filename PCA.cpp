40f8b6ff1f1d50f2357557f3d0b4283164ba5fc5
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-08-05 22:51:03 +0200

    Finished implementation. Everything is working.

--- PCA.cpp
+++ PCA.cpp
@@ -36,11 +36,8 @@ PCA::PCA(Texture *t)
 	cv::Mat img;	
 	cv::cvtColor(img1, img, CV_RGB2GRAY);
 
+	//calculate the main axis
 	calcPCA(img, this->m_x, this->m_y);
-	std::cout<<"("<<this->m_x<<","<<this->m_y<<")"<<std::endl;	
-	cv::PCA pca(img, cv::Mat(), CV_PCA_DATA_AS_ROW, 2);
-	std::cout<<pca.eigenvectors.rows<<"x"<<pca.eigenvectors.cols<<std::endl;
-	std::cout<<pca.eigenvectors.at<float>(0,0)<<"   "<<pca.eigenvectors.at<float>(1,0)<<std::endl;
 }
 
 PCA::PCA(const cv::Mat &t)
@@ -49,91 +46,8 @@ PCA::PCA(const cv::Mat &t)
 	cv::Mat img;	
 	cv::cvtColor(t, img, CV_RGB2GRAY);
 
-//-----------------------------------------------
-	cv::Mat test(2, 10, CV_32FC1);
-	//x
-	test.at<float>(0,0) = 2.5;
-	test.at<float>(0,1) = 0.5;
-	test.at<float>(0,2) = 2.2;
-	test.at<float>(0,3) = 1.9;
-	test.at<float>(0,4) = 3.1;
-	test.at<float>(0,5) = 2.3;
-	test.at<float>(0,6) = 2;
-	test.at<float>(0,7) = 1;
-	test.at<float>(0,8) = 1.5;
-	test.at<float>(0,9) = 1.1;
-
-	//y
-	test.at<float>(1,0) = 2.4;
-	test.at<float>(1,1) = 0.7;
-	test.at<float>(1,2) = 2.9;
-	test.at<float>(1,3) = 2.2;
-	test.at<float>(1,4) = 3.0;
-	test.at<float>(1,5) = 2.7;
-	test.at<float>(1,6) = 1.6;
-	test.at<float>(1,7) = 1.1;
-	test.at<float>(1,8) = 1.6;
-	test.at<float>(1,9) = 0.9;
-//----------------------------------------------
-	cv::Mat test2(2, img.reshape(0,1).cols,CV_32FC1);
-	for (int i = 0; i < img.reshape(0,1).cols; i++)
-	{
-		test2.at<float>(0, i) = img.reshape(0,1).at<uchar>(0, i);
-		test2.at<float>(1, i) = i;
-	}
-//----------------------------------------------
-	cv::Mat test3(3, img.cols * img.rows ,CV_32FC1);
-	for (int i = 0; i < img.cols * img.rows; i++)
-	{
-		test3.at<float>(0, i) = i / img.cols;
-		test3.at<float>(1, i) = i % img.rows;
-		test3.at<float>(2, i) = img.at<uchar>(i / img.cols, i % img.cols);
-	}
-//-------------------------------------------------
-
-
-//	calcPCA(img, this->m_x, this->m_y);
-
-//	cv::PCA pca(img.reshape(0,1), cv::Mat(), CV_PCA_DATA_AS_COL, 2);
-	cv::PCA pca(test3, cv::Mat(), CV_PCA_DATA_AS_COL, 2);
-
-	//return the first eigenvector
-	m_x = pca.eigenvectors.at<float>(0,0);
-	m_y = pca.eigenvectors.at<float>(0,1);
-
-//-----------------------Debug Output from here--------------------------------------------
-
-	std::cout<<pca.eigenvectors.rows<<"x"<<pca.eigenvectors.cols<<std::endl;
-	for (int j = 0; j<pca.eigenvectors.rows; j++)
-	{
-		for (int i = 0; i<pca.eigenvectors.cols; i++)
-		{
-			std::cout<<std::setw(14);
-			std::cout<<pca.eigenvectors.at<float>(j,i);
-		}
-		std::cout<<std::endl;
-	}
-	std::cout<<pca.eigenvalues.rows<<"x"<<pca.eigenvalues.cols<<std::endl;
-	for (int j = 0; j<pca.eigenvalues.rows; j++)
-	{
-		for (int i = 0; i<pca.eigenvalues.cols; i++)
-		{
-			std::cout<<std::setw(14);
-			std::cout<<pca.eigenvalues.at<float>(j,i);
-		}
-		std::cout<<std::endl;
-	}
-	
-/*		float a = 1000000;	
-		cv::line(img, cv::Point(img.cols/2,img.rows/2 ), cv::Point(a*m_x+img.cols/2, a*m_y+img.rows/2), cv::Scalar(0,0,0), 2);
-		cv::startWindowThread();
-		
-		//show the single pattern
-		cv::namedWindow("Pattern", CV_WINDOW_AUTOSIZE);
-		cv::imshow("Pattern", img);
-		cv::waitKey();
-*/
-//	std::cout<<"("<<this->m_x<<","<<this->m_y<<")"<<std::endl;	
+	//calculate the main axis
+	calcPCA(img, this->m_x, this->m_y);
 }
 
 float PCA::compareTo(PCA & other)
@@ -146,52 +60,24 @@ float PCA::compareTo(PCA & other)
 
 void PCA::calcPCA(cv::Mat isrc, float &x, float &y)
 {
-	//make one row vector from Matrix
-	cv::Mat img = isrc.reshape(0, 1);
-
-	float k11 = 0, k12 = 0, k21 = 0, k22 = 0;
-	
-	int N =  img.cols; 
-	float x_mean = (1.0f / N) * (N * (N - 1) / 2);
-	float y_mean = cv::mean(img)[0];
-	for (int i = 0; i < N; i++)
+	//build input vectors: First line = x values, second line = y values, third line = intensity
+	cv::Mat img(3, isrc.cols * isrc.rows ,CV_32FC1);
+	for (int i = 0; i < isrc.cols * isrc.rows; i++)
 	{
-		k11 += (i - x_mean) * (i - x_mean) / N;
-		k22 += (img.at<uchar>(0, i) - y_mean) * (img.at<uchar>(0, i) - y_mean) / N;
-		k12 += (i - x_mean) * (img.at<uchar>(0, i) - y_mean) / N;
+		img.at<float>(0, i) = i / isrc.cols;
+		img.at<float>(1, i) = i % isrc.rows;
+		img.at<float>(2, i) = isrc.at<uchar>(i / isrc.cols, i % isrc.cols);
 	}
-	k21 = k12;
-	std::cout<<k11<<" "<<k21<<" "<<k12<<" "<<k22<<std::endl;
-	
-	//set covariance matrix
-	cv::Mat C(2, 2, CV_32FC1);
-	C.at<float>(0,0) = k11;
-	C.at<float>(1,0) = k21;
-	C.at<float>(0,1) = k12;
-	C.at<float>(1,1) = k22;
-	
-	//get the eigenvectors of C
-	cv::Mat E, V;
-	cv::eigen(C, E, V);
+
+	//calculate PCA	
+	cv::PCA pca(img, cv::Mat(), CV_PCA_DATA_AS_COL, 2);
 
 	//return the first eigenvector
-	x = V.at<float>(0,0);
-	y = V.at<float>(1,0);
-	std::cout<<V.at<float>(0,0)<<" "<<V.at<float>(0,1)<<" "<<V.at<float>(1,0)<<" "<<V.at<float>(1,1)<<std::endl;
-/*	
-		float t = 10000;	
-		cv::line(isrc, cv::Point(0,0), cv::Point(t*x, t*y), cv::Scalar(0,0,0), 2);
-		cv::startWindowThread();
-		
-		//show the single pattern
-		cv::namedWindow("Pattern", CV_WINDOW_AUTOSIZE);
-		cv::imshow("Pattern", isrc);
-		cv::waitKey();
-*/
+	x = pca.eigenvectors.at<float>(0,0);
+	y = pca.eigenvectors.at<float>(0,1);
 }
 
 PCA::~PCA()
 {
-	//TODO?!
 }
 }
